66b682f0d2f226914d17ba8c5863bd8c35e80925
Trevor Manz <trevor.j.manz@gmail.com>
2026-04-06 16:06:40 -0400

    Use `tintype`

--- src/__tests__/vitest-setup.ts
+++
@@ -1,6 +0,0 @@
-import { expect } from "vitest";
-
-expect.addSnapshotSerializer({
-  test: (val) => val != null && typeof val === "object" && "__type__" in val,
-  serialize: (val) => (val as { __type__: string }).__type__,
-});
